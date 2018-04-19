4d31ac333186937f87e281a03e9adfa1ed59bb80
Kristine Sonu <ksonu@e1z2r9p48.42.us.org>
2018-04-19 16:33:14 -0700

    update

--- srcs/main.c
+++ srcs/main.c
@@ -5,43 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksonu <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/17 14:05:24 by ksonu             #+#    #+#             */
-/*   Updated: 2018/04/18 20:24:11 by ksonu            ###   ########.fr       */
+/*   Created: 2018/04/19 14:38:40 by ksonu             #+#    #+#             */
+/*   Updated: 2018/04/19 16:06:38 by ksonu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "mlx.h"
 #include <stdio.h>
 
 int		main(int ac, char **av)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_env	m;
 	int		fd;
-	t_env	*yap = NULL;
 
-	printf("aasd");
-	if (ac != 2)
+	if (ac ==2)
 	{
-		ft_putstr("usage : fdf input_file\n");
-		return (1);
+		fd = open(av[1], O_RDONLY);
+		read_max(fd, &m);
+		fd = open(av[1], O_RDONLY);
+		read_value(fd, &m);
 	}
-	if ((fd = (open(av[1], O_RDONLY))) < 0)
-	{
-		ft_putstr("error\n");
-		return (1);
-	}
-	else
-	{
-		printf("1234");
-		read_map_max(&av[1], yap);
-		printf("5678");
-		read_value(&av[1], yap);
-		printf("ymax[%d]", yap->y_max);
-	}
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 300, 300, "moonyoning");
-	mlx_loop(mlx_ptr);
-	return (0);
-}
+}	
--- srcs/read.c
+++ srcs/read.c
@@ -5,71 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksonu <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/18 13:50:11 by ksonu             #+#    #+#             */
-/*   Updated: 2018/04/18 20:15:11 by ksonu            ###   ########.fr       */
+/*   Created: 2018/04/19 14:52:57 by ksonu             #+#    #+#             */
+/*   Updated: 2018/04/19 16:31:49 by ksonu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "mlx.h"
 #include <stdio.h>
 
-void		read_map_max(char **av, t_env *yap)
+void		read_max(int fd, t_env *m)
 {
-	int		fd;
 	char	*line;
-	int		y_count;
-	int		x_count;
-	char	**split;
-
-	printf("1");
-	line = NULL;
-	y_count = 0;
-	x_count = 0;
-	yap->y_max = 0;
-	yap->x_max = 0;
-	fd = open(av[1], O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
-	{
-		y_count++;
-		split = ft_strsplit(line, ' ');
-		while (*split[x_count])
-			x_count++;
-		ft_strdel(&line);
-		ft_splitdel(split);
-	}
-	yap->y_max = y_count;
-	yap->x_max = x_count;
+	m->y_max = 0;
+	m->x_max = 0;
+		while (get_next_line(fd, &line) > 0)
+		{
+			m->y_max++;
+			m->x_max = ft_wdcount(line, ' ');
+			ft_strdel(&line);
+		}
 }
 
-void		read_value(char **av, t_env *yap)
+void		read_value(int fd, t_env *m)
 {
-	int		fd;
 	char	*line;
 	char	**split;
 	int		i;
-
-	printf("2");
+	int		j;
+	
 	i = 0;
-	line = NULL;
-	read_map_max(&av[1], yap);
-	fd = open(av[1], O_RDONLY);
-	//yap->pt = (t_point)malloc(sizeof(t_point) * ((yap->y_max) * (yap->x_max)));
-	yap->pt.y = 0;
-	while (get_next_line(fd, &line) > 0)
+	m->pt = (t_point**)malloc(sizeof(t_point*) * (m->y_max + 1));
+	while (i < m->y_max)
 	{
-		printf("3");
-		split = ft_strsplit(line, ' ');
-		while (split[i] != NULL)
+		if (get_next_line(fd, &line) == 1)
 		{
-			printf("4");
-			yap->pt.x = 0;
-			yap->pt.z = ft_atoi(split[i]);
-			i++;
-			yap->pt.x++;
+			m->pt[i] = (t_point*)malloc(sizeof(t_point) * (m->x_max + 1));
+			split = ft_strsplit(line, ' ');
+			j = 0;
+			while (j < m->x_max && split[j])
+			{
+				m->pt[i][j].x = ft_atoi(split[j]);
+				printf("m->pt[%d][%d].x : [%d]\n", i, j, m->pt[i][j].x);
+				j++;
+			}
+			ft_splitdel(split);
+			ft_strdel(&line);
 		}
-		yap->pt.y++;
-		ft_strdel(&line);
-		ft_splitdel(split);
+		i++;
 	}
 }
